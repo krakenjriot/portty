cbaf934dff83cf17a2fbc8fb6873e9b17f46c604
rollly <marclunaris@gmail.com>
2021-05-08 11:13:27 +0300

    init

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 		
 		
 		if($set == '1'){
-			file_put_contents("C:\\tmp\conf\b1.conf","00000001000000000000");
+			file_put_contents("C:\\tmp\conf\b1.output","00000000100000000000");
 			echo "the pin now is ON";
 		}		
 		
 		if($set == '0'){
-			file_put_contents("C:\\tmp\conf\b1.conf","00000000000000000000");
+			file_put_contents("C:\\tmp\conf\b1.output","00000000000000000000");
 			echo "the pin now is OFF";
 		}	
 	
@@ -22,7 +22,7 @@
 	}
 	
 	echo "</br>";
-	echo file_get_contents("C:\\tmp\conf\b1.conf");
+	echo file_get_contents("C:\\tmp\conf\b1.output");
 
 
 ?>
