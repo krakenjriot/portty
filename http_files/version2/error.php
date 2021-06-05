14fece8d2e111e5b7894182148e5723f9da3cbe3
rollly <marclunaris@gmail.com>
2021-06-05 15:07:04 +0300

    x

--- http_files/version2/error.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	echo "error page!";
-
-?>
--- http_files/version2/get_inputs.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	$config = include 'config';		
-	echo "<h2>".nl2br(file_get_contents($config['conf_dir']."\\".$config['board_name'].".input"))."</h2>";
-
-
